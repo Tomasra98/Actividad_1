af1f932b2ead6bf269ef42d7111b85edc57d2629
Tomasra98 <tomasramirezagudelo@gmail.com>
2023-08-07 15:37:27 -0500

    Implementación Ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+#Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla
+
+#Pedimos al usuario que ingrese su nombre
+
+nombre = input("Ingrese su nombre: ")
+
+#Pedimos al usuario que ingrese su edad
+
+edad = int(input("Ingrese su edad: "))
+
+#Mostramos los datos ingresados
+
+print("Hola,", nombre, "tienes", edad, "años.")
