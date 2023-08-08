cb7dbbdb67f1096db0554e27dd5dcb49dd9056f2
Tomasra98 <tomasramirezagudelo@gmail.com>
2023-08-07 20:14:32 -0500

    Implementación Ejercicio 13

---
+++ Ejercicio13.py
@@ -0,0 +1,10 @@
+#Crear un programa que pida al usuario ingresar dos números y calcule su suma, resta, multiplicación y división
+
+num1 = int(input("Digite un número: "))
+num2 = int(input("Digite otro número: "))
+
+print("Suma = ", num1 + num2)
+print("Resta = ", num1 - num2)
+print("Multiplicación = ", num1 * num2)
+print("División Exacta = ", num1 / num2)
+print("División Inexacta = ", num1 // num2)
